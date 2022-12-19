b568aa9c6678a2ddcf45834b516e3852bebe42e4
Tanush M M <tanushmmofficial@gmail.com>
2022-12-19 08:06:56 +0530

    Basic input and output in C++

---
+++ Basics/input_output.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // normal input and output in CPP
+    int d;
+    cout << "> ";
+    cin >> d;
+    cout << d << endl;
+
+    // multiple inputs in single line seperated by space
+    int a,b,c;
+    cout << "> ";
+    cin >>a>>b>>c;
+    cout <<a<<b<<c; 
+
+    return 0;
+}
